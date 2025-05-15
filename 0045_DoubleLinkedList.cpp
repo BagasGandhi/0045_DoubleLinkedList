f475ef856ade0b83fa815218ef875319e788b940
BagasGandhi <saputramuhammadgandhi@gmail.com>
2025-05-15 23:22:21 +0700

    12

--- 0045_DoubleLinkedList.cpp
+++ 0045_DoubleLinkedList.cpp
@@ -166,26 +166,3 @@ int main(){
         cout << "6. exit\n"; 
         cout << "Enter The Number: ";
         cin >> choice;
-
-        switch (choice)
-        {
-        case '1':
-            list.addNode();
-            break;
-        case '2':
-            list.hapus();
-            break;
-        case '3':
-            list.traverse();
-            break;
-        case '4':
-            list.revTraverse();
-            break;
-        case '5':
-            list.searchData();
-            break;
-        case '6':
-            return 0;
-        default:
-            cout << "Invalid Option\n";
-        }
